7c4073f7a8034692ff935a810f3c8611fe6033a9
Josh Patra <30350506+SoPat712@users.noreply.github.com>
2024-04-01 12:42:49 -0400

    updated plugins, disabled copilot

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -1,34 +1,34 @@
 return {
 
 	-- copilot
-	{
-		"zbirenbaum/copilot.lua",
-		cmd = "Copilot",
-		build = ":Copilot auth",
-		opts = {
-			suggestion = { enabled = false },
-			panel = { enabled = false },
-			filetypes = {
-				markdown = true,
-				help = true,
-			},
-		},
-	},
-	{
-		"nvim-lualine/lualine.nvim",
-		optional = true,
-		event = "VeryLazy",
-	},
+	-- {
+	-- 	"zbirenbaum/copilot.lua",
+	-- 	cmd = "Copilot",
+	-- 	build = ":Copilot auth",
+	-- 	opts = {
+	-- 		suggestion = { enabled = false },
+	-- 		panel = { enabled = false },
+	-- 		filetypes = {
+	-- 			markdown = true,
+	-- 			help = true,
+	-- 		},
+	-- 	},
+	-- },
+	-- {
+	-- 	"nvim-lualine/lualine.nvim",
+	-- 	optional = true,
+	-- 	event = "VeryLazy",
+	-- },
 
 	-- copilot cmp source
-	{
-		"nvim-cmp",
-		dependencies = {
-			{
-				"zbirenbaum/copilot-cmp",
-				dependencies = "copilot.lua",
-				opts = {},
-			},
-		},
-	},
+	-- {
+	-- 	"nvim-cmp",
+	-- 	dependencies = {
+	-- 		{
+	-- 			"zbirenbaum/copilot-cmp",
+	-- 			dependencies = "copilot.lua",
+	-- 			opts = {},
+	-- 		},
+	-- 	},
+	-- },
 }
